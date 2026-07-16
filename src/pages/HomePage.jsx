f77fc1dd29778ea46c64425fcaad023893c3611e
kyuwani <gyuwann@gmail.com>
2026-07-16 10:50:56 +0900

    feat: 메인 페이지 학교 생활 카테고리 버튼 추가

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,7 +7,7 @@ const HomePage = () => {
         <Header/>
         <div className="home_body">
             <div className="home_body_left">
-                <input className="search_var " type="search" placeholder="안녕하세요, 무엇을 알고 싶으세요?"/>
+                <input className="search_var " type="search" placeholder="검색어 입력"/>
                 <div className="school_life">
                     <h2>학교 생활</h2>
                     <table className="school_life_menu">
@@ -22,17 +22,15 @@ const HomePage = () => {
                                 <td><button>학생증</button></td>
                                 <td><button>졸업</button></td>
                             </tr>
-                            <tr>
-                                <td><button>국제교류</button></td>
-                                <td><button>증명서</button></td>
-                                <td><button>기숙사</button></td>
-                            </tr>
                         </tbody>
                     </table>
                     
                 </div>
                 <div className="body_left_bottom">
-                    <div className="campus_map_containor">캠퍼스 맵</div>
+                    <div className="campus_map_containor">
+                        <h2>캠퍼스 맵</h2>
+                        <div className="campus_map">캠퍼스 맵</div>
+                    </div>
                     <div className="chedule_calendar">일정 달력</div>
                 </div>
             </div>
